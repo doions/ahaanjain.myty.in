5e0d549957d213362e829122f781bffa93d9c44b
Doions <43123876+doions@users.noreply.github.com>
2020-01-23 19:17:19 +0530

    update config
    Basic config changes.

--- config/index.js
+++ config/index.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Ahaan Jain', // Navigation and Site Title
+  siteTitleAlt: 'Ahaan Jain - Personal Blog and Website', // Alternative Site title for SEO
+  siteTitleShort: 'Ahaan', // short_name for manifest
+  siteHeadline: 'Come & Enjoy our excellent moments from my life', // Headline for schema.org JSONLD
+  siteUrl: 'https://ahaanjain.myty.in', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
+  siteDescription: 'This is the personal website of Ahaan Jain, A six year old kid looking to express his journey in the form of a blog.',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
+  userTwitter: '@doions1', // Twitter Username
   ogSiteName: 'jodie', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
