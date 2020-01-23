e12ec1ee3e3ff1a141074aa2724525850539c9e0
Doions <43123876+doions@users.noreply.github.com>
2020-01-23 19:27:46 +0530

    New Bio
    New Bio with additional text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,13 +13,17 @@ const About = () => {
 
   return (
     <Layout>
-      <SEO title="About | Jodie" desc="Hi. I'm LekoArts! You can visit my website or my other Gatsby projects." />
+      <SEO title="About | Ahaan Jain" desc="Hi. I'm Ahaann! You can visit my website" />
       <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
-        <h1>Hi. I'm LekoArts!</h1>
+        <h1>Hi. I'm Ahaan!</h1>
         <p>
-          You can visit my <a href="https://www.lekoarts.de/en">website</a> or my other{' '}
-          <a href="https://gatsby-starter-portfolio.netlify.com">Gatsby projects</a>.
-        </p>
+          You can visit my <a href="https://ahaanjain.myty.in">website</a>
+          
+          I am a six year old boy studying in witty international school. I aspire to become an entrepreneur and a tech expert in the future.
+          I enjoy watching peep and chirp animation, ryan toy review, pink panthor.
+          My favorite activity is playing badminton, football and cricket with my friends.
+          I live in Malad (West),Mumbai.
+         </p>
       </AnimatedBox>
     </Layout>
   )
